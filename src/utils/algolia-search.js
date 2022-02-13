25b779e4e160262c35ebe37fcee67222085beb35
Sarah Kirby <sarahkirbyy@gmail.com>
2022-02-13 14:00:04 +1300

    Adds algolia-search utils, setups algoliaHelper instance

---
+++ src/utils/algolia-search.js
@@ -0,0 +1,30 @@
+import algoliasearch from 'algoliasearch';
+import algoliasearchHelper from 'algoliasearch-helper';
+
+const applicationID = '';
+const apiKey = '';
+const indexName = 'site-search';
+const client = algoliasearch(applicationID, apiKey);
+
+export const algoliaHelper = algoliasearchHelper(client, indexName, {
+  disjunctiveFacets: ['sectionName'],
+  hitsPerPage: 4,
+});
+
+export const searchCategories = {
+  courses: { hits: [], nbHits: 0 },
+  events: { hits: [], nbHits: 0 },
+  news: { hits: [], nbHits: 0 },
+};
+
+export const derivedHelpers = [
+  { category: 'courses', facet: 'Courses', helper: null },
+  { category: 'events', facet: 'Events', helper: null },
+  { category: 'news', facet: 'News', helper: null },
+];
+
+export const removeEventListeners = () => {
+  derivedHelpers.forEach((item) => {
+    item.helper.removeAllListeners('result');
+  });
+};
